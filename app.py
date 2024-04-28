7bb61bed0c5fd12acd0edc24df1e52a461db9caa
Lizzie Siegle <esiegle@brynmawr.edu>
2024-04-28 07:55:00 -0700

    parse llama result with max tokens

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
 from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import AstraDB as astra
+#from langchain_community.vectorstores import AstraDB as astra 
+from langchain_astradb import AstraDBVectorStore as astra 
 from openai import OpenAI
 import os
 import requests
 import streamlit as st
 from PIL import Image
+import json
 
 
 
 def encode_image(image_file):
     return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 # Load API secrets
 load_dotenv()
@@ -32,32 +35,107 @@ db = AstraDB(
 
 print(f"Connected to Astra DB: {db.get_collections()}")
 
-def query_llama(char_sim_search, species_sim_search, vehicle_sim_search, planet_sim_search, starship_sim_search):
-    prompt = f"Generate a Star Wars fanfiction story about a character like {char_sim_search} who meets a character of the {species_sim_search} species who drives a vehicle like {vehicle_sim_search}. To leave the planet, they encounter characters on a planet like {planet_sim_search} who leave on a starship like {starship_sim_search}"
-    response = requests.post(
-        f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct",
-        headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"},
-        json={
-            "messages": [
-                {"role": "system", "content": "You are a world-renowned Star Wars fanfiction writer."},
-                {"role": "user", "content": prompt}
-            ],
-            "max_tokens": 2000
-        }
-    )
-    result = response.json()
+url =f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/meta/llama-2-7b-chat-fp16"
+
+def query_llama1(char_sim_search, species_sim_search, vehicle_sim_search, planet_sim_search, starship_sim_search):
+    prompt = f"Return only a summarization of the Star Wars data contained in the following JSON objects. A character can only be from one planet: {char_sim_search} {species_sim_search} {vehicle_sim_search} {planet_sim_search} {starship_sim_search}"
+    print(f'prompt {prompt}')
+    payload = {
+        "max_tokens": 300,
+        "prompt": prompt,
+        "raw": False,
+        "stream": False
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    parsed_data = json.loads(response.text)
+    result = parsed_data['result']['response']
     print(result)
     return result
 
-# def query_hermes(payload):
-#   api_url = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/@hf/nousresearch/hermes-2-pro-mistral-7b"
-#   response = requests.post(api_url, headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"}, json=payload)
-#   return response.json()
+def query_llama2(res):
+    prompt = f"Return only a story based off of the following: {res}"
+    print(f'prompt {prompt}')
+    payload = {
+        "max_tokens": 9999,
+        "prompt": prompt,
+        "raw": False,
+        "stream": False
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    parsed_data = json.loads(response.text)
+    result2 = parsed_data['result']['response']
+    print(result2)
+    return result2
 
 def query_sdxl(payload):
   api_url = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/bytedance/stable-diffusion-xl-lightning"
   response = requests.post(api_url, headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"}, json=payload)
   return response.json()
+
+dataCache = {}
+
+char_docs = []
+veh_docs = []
+plan_docs = []
+ss_docs=[]
+species_docs = []
+@st.cache_data
+def loaddata(dataset):  #lizziepikachu/starwars_characters"
+    if dataset in dataCache.keys():
+        return dataCache[dataset]
+    data = load_dataset(dataset)["train"]
+    dataCache[dataset] = data
+    return data
+
+def ret_docs():
+    sw_char_huggingface_dataset = loaddata("lizziepikachu/starwars_characters")
+    sw_vehicle_huggingface_dataset = loaddata("lizziepikachu/starwars_vehicles")
+    sw_planet_huggingface_dataset = loaddata("lizziepikachu/starwars_planets")
+    sw_starship_huggingface_dataset = loaddata("lizziepikachu/starwars_starships")
+    sw_species_huggingface_dataset = loaddata("lizziepikachu/starwars_species")
+    print(f"An example entry from Hugging Face dataset: {sw_char_huggingface_dataset[0]}")
+    for entry in sw_char_huggingface_dataset:
+        metadata = {"name": entry["name"], "species": entry["species"], "gender": entry["gender"], "hair_color": entry["hair_color"], "homeworld": entry["homeworld"]}
+    
+        # Add a LangChain document with the name and metadata tags
+        doc = Document(page_content=entry["name"], metadata=metadata)
+        char_docs.append(doc)
+
+    for entry in sw_vehicle_huggingface_dataset:
+        metadata = {"name": entry["name"], "manufacturer": entry["manufacturer"], "cost_in_credits": entry["cost_in_credits"] }
+        # Add a LangChain document with the name and metadata tags
+        doc = Document(page_content=entry["name"], metadata=metadata)
+        veh_docs.append(doc)
+
+    for entry in sw_planet_huggingface_dataset:
+        metadata = {"name": entry["name"], "climate": entry["climate"], "terrain": entry["terrain"], "population": entry["population"] }
+        # Add a LangChain document with the name and metadata tags
+        doc = Document(page_content=entry["name"], metadata=metadata)
+        plan_docs.append(doc)
+
+    for entry in sw_starship_huggingface_dataset:
+        metadata = {"name": entry["name"], "manufacturer": entry["manufacturer"], "cost_in_credits": entry["cost_in_credits"] }
+        # Add a LangChain document with the name and metadata tags
+        doc = Document(page_content=entry["name"], metadata=metadata)
+        ss_docs.append(doc)
+    
+    for entry in sw_species_huggingface_dataset:
+        metadata = {"name": entry["name"], "classification": entry["classification"], "designation": entry["designation"], "language": entry["language"], "homeworld": entry["homeworld"]  }
+        # Add a LangChain document with the name and metadata tag
+        doc = Document(page_content=entry["name"], metadata=metadata)
+        species_docs.append(doc)
+
+print(f'char_docs {char_docs}, veh_docs {veh_docs}, plan_docs {plan_docs}, ss_docs {ss_docs}, species_docs {species_docs}') #
 
 
 # Set up Streamlit app
@@ -72,55 +150,10 @@ def main():
     vehicle_inp = st.text_input("Describe a vehicle you'd like to ride in")
     starship_inp = st.text_input("Describe a starship you'd like to ride in")
     species_inp = st.text_input("Describe a species you'd like to read about")
-    
     if char_inp is not None and vehicle_inp is not None and st.button('enter✅'):
         # load dataset once on page load/on server start
         with st.spinner('Processing input📈...'):
-            sw_char_huggingface_dataset = load_dataset("lizziepikachu/starwars_characters")["train"]
-            sw_vehicle_huggingface_dataset = load_dataset("lizziepikachu/starwars_vehicles")["train"]
-            sw_planet_huggingface_dataset = load_dataset("lizziepikachu/starwars_planets")["train"]
-            sw_starship_huggingface_dataset = load_dataset("lizziepikachu/starwars_starships")["train"]
-            sw_species_huggingface_dataset = load_dataset("lizziepikachu/starwars_species")["train"]
-            print(f"An example entry from Hugging Face dataset: {sw_char_huggingface_dataset[0]}")
-
-            char_docs = []
-            veh_docs = []
-            plan_docs = []
-            ss_docs=[]
-            species_docs = []
-            for entry in sw_char_huggingface_dataset:
-                metadata = {"name": entry["name"], "species": entry["species"], "gender": entry["gender"], "hair_color": entry["hair_color"], "homeworld": entry["homeworld"]}
-            
-                # Add a LangChain document with the name and metadata tags
-                doc = Document(page_content=entry["name"], metadata=metadata)
-                char_docs.append(doc)
-
-            for entry in sw_vehicle_huggingface_dataset:
-                metadata = {"name": entry["name"], "manufacturer": entry["manufacturer"], "cost_in_credits": entry["cost_in_credits"] }
-                # Add a LangChain document with the name and metadata tags
-                doc = Document(page_content=entry["name"], metadata=metadata)
-                veh_docs.append(doc)
-
-            for entry in sw_planet_huggingface_dataset:
-                metadata = {"name": entry["name"], "climate": entry["climate"], "terrain": entry["terrain"], "population": entry["population"] }
-                # Add a LangChain document with the name and metadata tags
-                doc = Document(page_content=entry["name"], metadata=metadata)
-                plan_docs.append(doc)
-
-            for entry in sw_starship_huggingface_dataset:
-                metadata = {"name": entry["name"], "manufacturer": entry["manufacturer"], "cost_in_credits": entry["cost_in_credits"] }
-                # Add a LangChain document with the name and metadata tags
-                doc = Document(page_content=entry["name"], metadata=metadata)
-                ss_docs.append(doc)
-            
-            for entry in sw_species_huggingface_dataset:
-                metadata = {"name": entry["name"], "classification": entry["classification"], "designation": entry["designation"], "language": entry["language"], "homeworld": entry["homeworld"]  }
-                # Add a LangChain document with the name and metadata tag
-                doc = Document(page_content=entry["name"], metadata=metadata)
-                species_docs.append(doc)
-
-            print(f'char_docs {char_docs}, veh_docs {veh_docs}, plan_docs {plan_docs}, ss_docs {ss_docs}, species_docs {species_docs}') #
-            
+            ret_docs()
             embedding_function = OpenAIEmbeddings()
             vstore_char = astra(
                 embedding=embedding_function,
@@ -181,23 +214,25 @@ def main():
             # })
             # story = story['result']["response"]
             # print(story)
-            story = query_llama(char_sim_search, species_sim_search, vehicle_sim_search, planet_sim_search, starship_sim_search)
+            sum_query = query_llama1(char_sim_search, species_sim_search, vehicle_sim_search, planet_sim_search, starship_sim_search)
+            print(sum_query)
+            story = query_llama2(sum_query)
             print(story)
 
-            img = query_sdxl({
-                "prompt": f"You are a world-renowned painter of Star Wars-related art. Generate an image about a character like {char_sim_search} in front of a vehicle like {vehicle_sim_search}" # or a starship like {starship_sim_search} 
+            # img = query_sdxl({
+            #     "prompt": f"You are a world-renowned painter of Star Wars-related art. Generate an image about a character like {char_sim_search} in front of a vehicle like {vehicle_sim_search}" # or a starship like {starship_sim_search} 
 
-            })
-            print(img)
-            # img = Image.open(img)
-            st.image(img)
+            # # })
+            # print(img)
+            # # img = Image.open(img)
+            # st.image(img)
             
             html_str = f"""
             <p style="font-family:Arial; color:Pink; font-size: 16px;">Story: {story}</p>
             """
             st.markdown(html_str, unsafe_allow_html=True)
-    st.write("Made w/ ❤️ in SF 🌁")
-    st.write("✅ out the [code on GitHub](https://github.com/elizabethsiegle/strava-astradb-sim-search/blob/main/app2.py)")
+    st.write("Made w/ ❤️ in Hawaii 🏝️🌺")
+    st.write("✅ out the [code on GitHub](https://github.com/elizabethsiegle/star-wars-fanfic-generator-streamlit-astra-cf)")
 
 
 if __name__ == "__main__":
